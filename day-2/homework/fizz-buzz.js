1a92ccc5658b4e77befb97cfe517d654e0594ed6
darrenheine <darren.p.heine@gmail.com>
2024-05-21 15:58:12 -0500

    updated loops

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -1,6 +1,6 @@
 // #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 /**
  * 2: Fizz Buzz
@@ -9,22 +9,29 @@
  *  b. BUT, for multiples of three print “Fizz” instead of the number.
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
-*/
+ */
 function fizzBuzz() {
-    // YOUR CODE GOES BELOW HERE //
+  // YOUR CODE GOES BELOW HERE //
+  for (var i = 1; i <=100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+  }
 
-
-
-    // YOUR CODE GOES ABOVE HERE //
+  // YOUR CODE GOES ABOVE HERE //
 }
 
-
-
-
-
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
-   (typeof process.versions.node !== 'undefined')) {
-    // here, export any references you need for tests //
-    module.exports.fizzBuzz = fizzBuzz
+if (
+  typeof process !== "undefined" &&
+  typeof process.versions.node !== "undefined"
+) {
+  // here, export any references you need for tests //
+  module.exports.fizzBuzz = fizzBuzz;
 }
