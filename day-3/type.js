4fa32b273be7d50525fee617fe2d1a4aff09e1f2
darrenheine <darren.p.heine@gmail.com>
2024-05-22 15:55:37 -0500

    completed day 3 of scratch pad

--- day-3/type.js
+++ day-3/type.js
@@ -1,67 +1,74 @@
 // #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 /**
  * IN CLASS EXERCISE: TYPE
  */
 
-/** 
+/**
  * Given an input value, return true if the value is an Array, false if otherwise.
- * 
+ *
  * TIP: In JavaScript, how can we decipher if a value is an Array? Can typeof
  * work?
- * 
+ *
  * HINT: There is a method that can help with this.
  */
 function isArray(value) {
-    // YOUR CODE BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+
+  return Array.isArray(value);
+
+  // YOUR CODE ABOVE HERE //
 }
 
-/** 
- * Given an input value, return true if the value is an Object intended as a 
+/**
+ * Given an input value, return true if the value is an Object intended as a
  * collection, false if otherwise.
- * 
- * TIP: In JavaScript, how can we decipher if a value is an Object, but not 
- * null, not an Array, not a Date - all of these will return 'object' if used 
+ *
+ * TIP: In JavaScript, how can we decipher if a value is an Object, but not
+ * null, not an Array, not a Date - all of these will return 'object' if used
  * with typeof.
- * 
+ *
  */
 function isObject(value) {
-    // YOUR CODE BELOW HERE //
-    
-
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+  if (
+    typeof value === "object" &&
+    value !== null &&
+    !Array.isArray(value) &&
+    !(value instanceof Date)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+  // YOUR CODE ABOVE HERE //
 }
 
-
-
-/** 
- * Given an input value, return true if is either an Array or an an Object 
+/**
+ * Given an input value, return true if is either an Array or an an Object
  * intended as a collection, false if otherwise.
- * 
+ *
  * TIP: Similar to isObject, but we must return true if the value is an Array.
  */
 function isCollection(value) {
-    // YOUR CODE BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+  if (
+    Array.isArray(value) ||
+    (typeof value === "object" && value !== null && !(value instanceof Date))
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+  // YOUR CODE ABOVE HERE //
 }
 
 /**
  * Given an input value, return the type of the value as a String
- * 
- * Types are one of: 
+ *
+ * Types are one of:
  *    - "string"
  *    - "array"
  *    - "object"
@@ -71,27 +78,52 @@ function isCollection(value) {
  *    - "null"
  *    - "function"
  *    - "date"
- * 
+ *
  * Examples:
  *    typeOf(134) -> "number"
  *    typeOf("javascript") -> "string"
  *    typeOf([1,2,3]) -> "array"
- */ 
+ */
 function typeOf(value) {
-    // YOUR CODE BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+  if (Array.isArray(value)) {
+    return "array";
+  }
+  if (value === null) {
+    return "null";
+  }
+  if (typeof value === "string") {
+    return "string";
+  }
+  if (typeof value === "number") {
+    return "number";
+  }
+  if (typeof value === "boolean") {
+    return "boolean";
+  }
+  if (typeof value === "function") {
+    return "function";
+  }
+  if (value instanceof Date) {
+    return "date";
+  }
+  if (typeof value === "object") {
+    return "object";
+  }
+  if (typeof value === "undefined") {
+    return "undefined";
+  }
+  // YOUR CODE ABOVE HERE //
 }
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
-    (typeof process.versions.node !== 'undefined')) {
-    // here, export any references you need for tests //
-    module.exports.isArray = isArray;
-    module.exports.isObject = isObject;
-    module.exports.isCollection = isCollection;
-    module.exports.typeOf = typeOf;
+if (
+  typeof process !== "undefined" &&
+  typeof process.versions.node !== "undefined"
+) {
+  // here, export any references you need for tests //
+  module.exports.isArray = isArray;
+  module.exports.isObject = isObject;
+  module.exports.isCollection = isCollection;
+  module.exports.typeOf = typeOf;
 }
