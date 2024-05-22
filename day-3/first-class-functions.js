c9e040bf0f33d5f6c192e2869cd48f8d59e9176c
darrenheine <darren.p.heine@gmail.com>
2024-05-22 15:21:08 -0500

    completed first-class-functions

--- day-3/first-class-functions.js
+++ day-3/first-class-functions.js
@@ -1,111 +1,134 @@
 // #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 /**
  * IN CLASS EXERCISE: FIRST CLASS FUNCTIONS
  */
 
-/** 
- * Given an input base to test against, which could be a String or Number, 
- * return a Function that tests whether a given value is greater than the 
+/**
+ * Given an input base to test against, which could be a String or Number,
+ * return a Function that tests whether a given value is greater than the
  * base.
  */
 function createGreaterThanFilter(base) {
-    // YOUR CODE BELOW HERE //
-    
-   
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+
+  return function (value) {
+    return value > base;
+  };
+
+  // YOUR CODE ABOVE HERE //
 }
 
-/** 
- * Given an input base to test against, which could be a String or Number, 
- * return a Function that tests whether a given value is less than the 
+/**
+ * Given an input base to test against, which could be a String or Number,
+ * return a Function that tests whether a given value is less than the
  * base. (test means return true or false)
  */
 function createLessThanFilter(base) {
-    // YOUR CODE BELOW HERE //
-    
-   
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+
+  return function (value) {
+    return value < base;
+  };
+
+  // YOUR CODE ABOVE HERE //
 }
 
-/** 
- * Given a startsWith character, which will be a single character, return a 
- * Function that tests whether a given String starts with the startsWith 
+/**
+ * Given a startsWith character, which will be a single character, return a
+ * Function that tests whether a given String starts with the startsWith
  * character.
  */
 function createStartsWithFilter(startsWith) {
-    // YOUR CODE BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+  let lowerCaseStartsWith = startsWith.toLowerCase();
+  return function (string) {
+    return string[0].toLowerCase() === lowerCaseStartsWith;
+  };
+
+  // YOUR CODE ABOVE HERE //
 }
 
-/** 
- * Given a endsWith character, which will be a single character, return a 
- * Function that tests whether a given String ends with the endsWith 
+/**
+ * Given a endsWith character, which will be a single character, return a
+ * Function that tests whether a given String ends with the endsWith
  * character.
  */
 function createEndsWithFilter(endsWith) {
-    // YOUR CODE BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+  let lowerCaseEndsWith = endsWith.toLowerCase();
+  return function (string) {
+    return string[string.length - 1].toLowerCase() === lowerCaseEndsWith;
+  };
+  // YOUR CODE ABOVE HERE //
 }
 
-/** 
- * Given an Array of Strings and a Function designed to modify a String, 
+/**
+ * Given an Array of Strings and a Function designed to modify a String,
  * return the Array of the Strings, modified.
- * 
- * TIP: You need to loop over the Strings, right? We need to pass each String to 
+ *
+ * TIP: You need to loop over the Strings, right? We need to pass each String to
  * the modify Function, but we need to collect the results into some collection.
  */
 function modifyStrings(strings, modify) {
-    // YOUR CODE BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+  const modifiedStrings = [];
+  for (var i = 0; i < strings.length; i++) {
+    modifiedStrings.push(modify(strings[i]));
+  }
+  return modifiedStrings;
+  // YOUR CODE ABOVE HERE //
 }
 
+modifyStrings(["alex", "francis", "aaron"], function (string) {
+  return string + "!";
+});
+// RETURNS => ['alex!', 'francis!', 'aaron!']
 
+modifyStrings(["alex", "francis", "aaron"], function (string) {
+  return string.toUpperCase();
+});
+// RETURNS => ['ALEX', 'FRANCIS', 'AARON']
 
-/** 
- * Given an Array of Strings and a Function designed to test the String in some 
+/**
+ * Given an Array of Strings and a Function designed to test the String in some
  * way and return a Boolean on whether it passed, return true if ALL Strings pass the test.
- * 
- * Imagine you had a list of names, and you wanted to test they all 
+ *
+ * Imagine you had a list of names, and you wanted to test they all
  * begin with "C", or they are all exclaimations that end with "!".
- * 
+ *
  * TIP: You need to loop over the Strings, right? And pass them to the test?
  */
 function allStringsPass(strings, test) {
-    // YOUR CODE BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE ABOVE HERE //
+  // YOUR CODE BELOW HERE //
+  for (var i = 0; i < strings.length; i++) {
+    if (!test(strings[i])) {
+        return false;
+    }
+  }
+  return true;
+  // YOUR CODE ABOVE HERE //
 }
 
+// RETURNS => true (because all strings begin with 'a')
+
+allStringsPass(["alex", "francis", "aaron"], function (string) {
+  return string[0] === "a";
+});
+// RETURNS => false (because one of the strings doesn't begin with 'a')
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
-    (typeof process.versions.node !== 'undefined')) {
-    // here, export any references you need for tests //
-    module.exports.createGreaterThanFilter = createGreaterThanFilter;
-    module.exports.createLessThanFilter = createLessThanFilter;
-    module.exports.createStartsWithFilter = createStartsWithFilter;
-    module.exports.createEndsWithFilter = createEndsWithFilter;
-    module.exports.modifyStrings = modifyStrings;
-    module.exports.allStringsPass = allStringsPass;   
+if (
+  typeof process !== "undefined" &&
+  typeof process.versions.node !== "undefined"
+) {
+  // here, export any references you need for tests //
+  module.exports.createGreaterThanFilter = createGreaterThanFilter;
+  module.exports.createLessThanFilter = createLessThanFilter;
+  module.exports.createStartsWithFilter = createStartsWithFilter;
+  module.exports.createEndsWithFilter = createEndsWithFilter;
+  module.exports.modifyStrings = modifyStrings;
+  module.exports.allStringsPass = allStringsPass;
 }
