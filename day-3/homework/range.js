a1962a30db0741cb6424047d737df4cc8bc04b35
darrenheine <darren.p.heine@gmail.com>
2024-05-28 12:18:49 -0500

    completed scratch pad

--- day-3/homework/range.js
+++ day-3/homework/range.js
@@ -1,17 +1,17 @@
 // #!/usr/bin/env node
 
-'use strict';
+"use strict";
 
 /**
  * 3: range()
  *
- *  a. Create a function called range that takes two integers as 
- *  parameters and returns an Array containing all integers 
+ *  a. Create a function called range that takes two integers as
+ *  parameters and returns an Array containing all integers
  *  between the two parameters, inclusively.  For example:
  *
  *      range(8, 14);  // => [8, 9, 10, 11, 12, 13, 14]
  *
- *  If the first argument is greater than the second, 
+ *  If the first argument is greater than the second,
  *  return the range in reverse order, ie:
  *
  *      range(14, 8);  // => [14, 13, 12, 11, 10, 9, 8]
@@ -22,21 +22,27 @@
  *  c. you'll wanna make use of the push() method of Array.
  */
 function range(start, end) {
-    // YOUR CODE GOES BELOW HERE //
-    
-    
-    
-    
-    // YOUR CODE GOES ABOVE HERE //
+  // YOUR CODE GOES BELOW HERE //
+  let result = [];
+  if (start <= end) {
+    for (let i = start; i <= end; i++) {
+      result.push(i);
+    }
+  } else {
+    for (let i = start; i >= end; i--) {
+      result.push(i);
+    }
+  }
+  return result;
+
+  // YOUR CODE GOES ABOVE HERE //
 }
 
-
-
-
-
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
-   (typeof process.versions.node !== 'undefined')) {
-    // here, export any references you need for tests //
-    module.exports.range = range;
+if (
+  typeof process !== "undefined" &&
+  typeof process.versions.node !== "undefined"
+) {
+  // here, export any references you need for tests //
+  module.exports.range = range;
 }
